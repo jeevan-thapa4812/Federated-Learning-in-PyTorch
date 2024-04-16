adb43d942bbd14c2e9ec09e2cca8abec54c88bbe
jeevan11vision <jeevanthapa9111@gmail.com>
2024-04-16 18:54:55 -0400

    Resolve momentum-betas issue for fedadam

--- src/server/fedadamserver.py
+++ src/server/fedadamserver.py
@@ -1,17 +1,28 @@
 import logging
+from collections import defaultdict
 
 from .fedavgserver import FedavgServer
 
 logger = logging.getLogger(__name__)
 
 
-
 class FedadamServer(FedavgServer):
-    def __init__(self, **kwargs):
-        super(FedadamServer, self).__init__(**kwargs)
+    def __init__(self, args, writer, server_dataset, client_datasets, model):
+        super(FedavgServer, self).__init__()
+        self.args = args
+        self.writer = writer
+
+        self.round = 0  # round indicator
+        if self.args.eval_type != 'local':  # global holdout set for central evaluation
+            self.server_dataset = server_dataset
+        self.global_model = self._init_model(model)  # global model
         self.opt_kwargs = dict(
-            betas=(self.args.beta1, self.args.beta2), 
-            v0=self.args.tau**2,
+            betas=(self.args.beta1, self.args.beta2),
+            v0=self.args.tau ** 2,
             tau=self.args.tau,
             lr=self.args.server_lr
         )
+        self.curr_lr = self.args.lr  # learning rate
+        self.clients = self._create_clients(client_datasets)  # clients container
+        self.results = defaultdict(dict)  # logging results container
+        self.server_optimizer = self._get_algorithm(self.global_model, **self.opt_kwargs)
